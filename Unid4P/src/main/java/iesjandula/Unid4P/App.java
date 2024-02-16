d92e6a366c47fdc48f1c83f480560d0ebf210649
2DAW <2DAW@2DAM-19>
2024-02-16 14:41:37 +0100

    commit 16/02/2024

---
+++ Unid4P/src/main/java/iesjandula/Unid4P/App.java
@@ -0,0 +1,13 @@
+package iesjandula.Unid4P;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
